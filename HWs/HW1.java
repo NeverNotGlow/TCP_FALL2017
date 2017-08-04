93717ce3d8d81c4c4001522589c3149265471c12
Infidelishious <i.m.glow101@gmail.com>
2017-08-04 14:03:11 -0700

    Uploaded HW1

---
+++ HWs/HW1.java
@@ -0,0 +1,30 @@
+/*  
+    Assignment: HW1 - Hello World
+    Author: [YOUR NAME]
+
+    Note: to make this file easier to read you can turn on/off wordwrap in VSCode by pressing ALT+Z
+
+    Description :
+        In your first assignment you will show me that you have your development environment set up an you can interact with it.
+
+        In this assingment you will be creating your own version of the "Hello World" program we talked about in class today and send me a screenshot of it.
+
+    Steps:
+        1) Recreate the hello world program from today's class on your local computer in this file below the end of this block comment.
+
+        2) Change the text to output "Hello World, I am [YOUR NAME]!" where [YOUR NAME] is replaced with your actual name first and last.
+
+        3) Compile and run the program, if you need help look at the slides. Make sure it outputs the Hello world text.
+
+        4) Take a screenshot of the command window with the output in it, save the screenshot as HW1.jpg and upload to Moodle along with your modified version of this file.
+
+    Grading:
+        50% - Screenshot of "Hello World, I am [YOUR NAME]!"
+        50% - modified HW1.java that compiles and outputs desired text
+
+    Required files in your submission:
+        - HW1.java that you have edited (NOT THE HW1.class)
+        - HW1.jpg taken by the snipping tool in Windows 
+*/
+
+////// Do ASSIGNMENT BELOW /////////
